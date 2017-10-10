5bfe391f766a6237869f747bb7b1cdea0c427ebc
amberwoo <31637286+amberwoo@users.noreply.github.com>
2017-10-10 15:01:48 -0400

    update

---
+++ creative-computing/javascript/javascript.js
@@ -0,0 +1,160 @@
+for (var i = 0; i < 10; i = i + 1) {
+    for (var j = 0; j < 10; j = j + 1) {
+        console.log("Row " + i + ", Column " + j);
+
+        // The next line is the only JS you'll have to change.
+        if (i < 5) {
+            console.log("   pink");
+            document.write("<div class='box pink'></div>");
+        } else {
+            console.log("   turquoise");
+            document.write("<div class='box turquoise'></div>");
+        }
+    }
+
+    document.write('<br />');
+}
+
+document.write('<br />');
+
+
+for (var i = 0; i < 10; i = i + 1) {
+    for (var j = 0; j < 10; j = j + 1) {
+        console.log("Row " + i + ", Column " + j);
+
+        // The next line is the only JS you'll have to change.
+        if (j < 5) {
+            console.log("   pink");
+            document.write("<div class='box pink'></div>");
+        } else {
+            console.log("   turquoise");
+            document.write("<div class='box turquoise'></div>");
+        }
+    }
+
+    document.write('<br />');
+}
+
+document.write('<br />');
+
+
+
+for (var i = 0; i < 10; i = i + 1) {
+    for (var j = 0; j < 10; j = j + 1) {
+        console.log("Row " + i + ", Column " + j);
+
+        // The next line is the only JS you'll have to change.
+        if (j == i) {
+            console.log("   pink");
+            document.write("<div class='box pink'></div>");
+        } else {
+            console.log("   turquoise");
+            document.write("<div class='box turquoise'></div>");
+        }
+    }
+
+    document.write('<br />');
+}
+
+document.write('<br />');
+
+
+
+for (var i = 0; i < 10; i = i + 1) {
+    for (var j = 0; j < 10; j = j + 1) {
+        console.log("Row " + i + ", Column " + j);
+
+        // The next line is the only JS you'll have to change.
+        if (j < i+1) {
+            console.log("   pink");
+            document.write("<div class='box pink'></div>");
+        } else {
+            console.log("   turquoise");
+            document.write("<div class='box turquoise'></div>");
+        }
+    }
+
+    document.write('<br />');
+}
+
+document.write('<br />');
+
+
+
+for(var k=0;k<10;k = k + 1){
+    if(k%2==0){
+        for(var i = 0;i<10;i = i + 1){
+            document.write("<div class='box pink'></div>");
+        }
+    }else{
+        for(var j = 0;j<10;j = j + 1){
+            document.write("<div class='box turquoise'></div>");
+        }
+    }
+    document.write('<br />');
+}
+document.write('<br />');
+
+
+for (var i = 0; i < 10; i = i + 1) {
+    for (var j = 0; j < 10; j = j + 1) {
+        console.log("Row " + i + ", Column " + j);
+
+        // The next line is the only JS you'll have to change.
+        if (i < j + 1) {
+            console.log("   pink");
+            document.write("<div class='box pink'></div>");
+        } else {
+            console.log("   turquoise");
+            document.write("<div class='box turquoise'></div>");
+        }
+    }
+
+    document.write('<br />');
+}
+
+document.write('<br />');
+
+
+for (var i = 0; i < 10; i = i + 1) {
+    for (var j = 0; j < 10; j = j + 1) {
+        console.log("Row " + i + ", Column " + j);
+
+        // The next line is the only JS you'll have to change.
+        if (i / 5 == 1) {
+            console.log("   pink");
+            document.write("<div class='box pink'></div>");
+        } else {
+            console.log("   turquoise");
+            document.write("<div class='box turquoise'></div>");
+        }
+    }
+
+    document.write('<br />');
+}
+
+document.write('<br />');
+
+
+
+
+for (var i = 0; i < 10; i = i + 1) {
+    for (var j = 0; j < 10; j = j + 1) {
+        console.log("Row " + i + ", Column " + j);
+
+        // The next line is the only JS you'll have to change.
+        if (j == 2) {
+            console.log("   pink");
+            document.write("<div class='box pink'></div>");
+        } else {
+            console.log("   turquoise");
+            document.write("<div class='box turquoise'></div>");
+        }
+    }
+
+    document.write('<br />');
+}
+
+document.write('<br />');
+
+
