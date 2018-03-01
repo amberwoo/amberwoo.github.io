7e7e5f9e9ffc81e33c874a636319e79ca3fdae8d
amberwoo <31637286+amberwoo@users.noreply.github.com>
2018-03-01 14:10:04 -0500

    1

---
+++ collection2/lv.js
@@ -0,0 +1,13 @@
+// // get text
+// var p4 = document.getElementById("p4")
+//
+// // get image
+// var h4 = document.getElementById("h4")
+//
+// // add click event listener to image
+// h4.addEventListener("mouseover", handleH4Moov)
+//
+// function handleH4Moov() {
+//
+// 	p4.style.display = "block";
+// }
