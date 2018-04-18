4d78f49f8b77e6af2d050513f3ef1c9b217db0b2
amberwoo <31637286+amberwoo@users.noreply.github.com>
2018-04-17 20:55:39 -0400

    1

---
+++ exercise.js
@@ -0,0 +1,8 @@
+var back = document.getElementById("back")
+var mickey = document.getElementById("mickey")
+
+back.addEventListener("click", handleBackClick)
+
+function handleBackClick() {
+mickey.style.display = "block";
+}
